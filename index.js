74da1fb86fc5f7b16cb4f9495c124560ce4e670a
Mehedi Hasan <mehedihasan.codes3@gmail.com>
2026-01-08 00:10:25 +0600

    solved problem 2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Problem 1: Reverse a String
 
 function reverseString(str) {
-    let result = '';
+    let result = "";
     for (let i = str.length - 1; i >= 0; i--) {
         result = result + str[i];
     }
@@ -10,3 +10,16 @@ function reverseString(str) {
 // console.log(reverseString("hello"));
 
 
+// Problem 2: Count Vowels in a String
+
+function countVowels(str) {
+    let count = 0;
+    const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str[i])) {
+            count++;
+        }
+    }
+    return count;
+}
+console.log(countVowels("Programming"));
